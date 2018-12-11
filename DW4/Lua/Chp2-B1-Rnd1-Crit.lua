3ed52328d2fadea6941353468738e8185ee86e64
adelikat <adelikat@tasvideos.org>
2018-12-10 20:33:10 -0600

    start boss script at a later point, 2nd rng value can be manipulated while being walked to the alter

--- DW4/Lua/Chp2-B1-Rnd1-Crit.lua
+++ DW4/Lua/Chp2-B1-Rnd1-Crit.lua
@@ -6,9 +6,20 @@ c.maxDelay = 0
 
 while not c.done do
 	c.Load(0)
-	c.RndWalkingFor('Up', 145)
-	c.PushA()
-	
+	c.RndWalkingFor('Up', 520)
+	c.WaitFor(652)
+	c.RandomFor(1)
+	local bail = false
+		if emu.islagged() then
+			c.Debug('Lagged on magic frame, aborting')
+			bail = true
+		end
+
+	if bail == false then
+		c.WaitFor(14)
+		c.RndAtLeastOne()
+	end
+
 	c.WaitFor(100)
 	c.Increment()
 end
