332f6386c98c4066f99eac130deab91c61341270
adelikat <AndrewJDelikat@gmail.com>
2023-02-27 10:51:09 -0600

    DW4 - Up to Linguar

---
+++ DW4/Lua/Chp2/Chp2-Sampson-Rnd1.lua
@@ -0,0 +1,74 @@
+local c = require("DW4-ManipCore")
+c.InitSession()
+c.reportFrequency = 100
+c.maxDelay = 0
+
+function _sampsonFirst()
+	c.RandomFor(1)
+	c.WaitFor(2)
+	c.UntilNextInputFrame()
+	c.WaitFor(2)
+	c.RndAtLeastOne() -- Sampson appears
+	c.RandomFor(19)
+	c.UntilNextInputFrame()
+	c.WaitFor(2) -- Input frame
+	c.UntilNextInputFrame()
+	c.PushA() -- Attack
+	c.WaitFor(3)
+	c.UntilNextInputFrame()
+
+    c.WaitFor(1) -- No delay was proven impossible
+    c.DelayUpTo(2)
+	c.PushA() -- Pick Sampson
+	c.RandomFor(28)    
+	c.UntilNextInputFrame()
+	
+	if c.ReadTurn() ~= 4 then
+		return c.Bail('Sampson did not go first')
+	end
+
+	if c.ReadBattle() ~= 76 then
+		return c.Bail('Sampson did not attack, not sure what he did')
+	end
+
+	c.WaitFor(2)
+
+	return true
+end
+
+function _miss()
+	local origHp = c.Read(c.Addr.AlenaHP)
+	c.RndAtLeastOne()
+	c.WaitFor(5)
+
+	local currHp = c.Read(c.Addr.AlenaHP)
+	local dmg = origHp - currHp
+	c.Debug('dmg: ' .. dmg)
+	return dmg == 0
+end
+
+c.Load(0)
+c.Save(100)
+c.RngCacheClear()
+while not c.done do
+	c.Load(100)
+	local result = c.Cap(_sampsonFirst, 1000)
+	if result then
+		--c.Log('Turn manipulated')
+		local cacheResult = c.AddToRngCache()
+		if cacheResult then
+			local result = c.Cap(_miss, 2)
+			if result then
+				c.Done()
+			else
+				c.Log('Could not get a miss')
+			end
+		else
+			c.Log('RNG already found')
+		end
+	else
+		c.Log('Unable to manip turn')
+	end
+end
+
+c.Finish()
--- DW4/Lua/OneOff.lua
+++ DW4/Lua/OneOff.lua
@@ -1,6 +1,6 @@
 local c = require("DW4-ManipCore")
 c.InitSession()
-c.reportFrequency = 100
+c.reportFrequency = 10000
 
 local function _do()
 	c.RndAorB()
@@ -14,12 +14,24 @@ local function _do()
 	return true
 end
 
+function _critical()
+    c.RndAtLeastOne()
+    c.WaitFor(4)
+	local dmg = c.ReadDmg()
+	c.Debug('Dmg: ' .. dmg)
+    return dmg > 20
+end
+
 c.Load(0)
 c.Save(100)
-while not c.done do	
+while not c.done do
 	c.Load(100)
-	c.Best(_do, 100)
-	c.Done()
+	--local result = c.RngSearch(_critical)
+	local result = c.FrameSearch(_critical, 1000)
+	if result then
+		c.Done()
+	end
+	c.Log('Critical is impossible!')
 end
 
 c.Finish()
