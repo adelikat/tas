30f37dfa65f301c676a64f59778debeb485100e5
adelikat <AndrewJDelikat@gmail.com>
2023-03-12 10:50:12 -0500

    DW4 - Chp3 - Enter Endor with 7 Broad swords, 2131 frames ahead

--- DW4/Lua/OneOff.lua
+++ DW4/Lua/OneOff.lua
@@ -4,18 +4,29 @@ c.reportFrequency = 10000
 local stat = c.Addr.TaloonVit
 
 
-local function _readOffer()
-    local offer = memory.read_u16_le(0x006F)
-    c.Debug('offer: ' .. offer)
-    return offer
-end
-
 local function _do()
-	c.PushA()
+	c.RndAorB()
 	c.WaitFor(10)
-	local dmg = c.ReadDmg()
-	c.Log('dmg: ' .. dmg)
-	return false
+	c.UntilNextInputFrame()
+
+	c.RndAorB()
+	c.WaitFor(10)
+	c.UntilNextInputFrame()
+
+	c.RndAorB()
+	c.WaitFor(10)
+	c.UntilNextInputFrame()
+
+	c.WaitFor(1)
+	c.RndAtLeastOne()
+	c.WaitFor(10)
+	c.UntilNextInputFrame()
+
+	c.PushUp()
+	c.RandomFor(20)
+	c.UntilNextInputFrame()
+	
+	return true
 end
 
 c.Load(0)
@@ -23,9 +34,9 @@ c.Save(100)
 c.RngCacheClear()
 while not c.done do
 	c.Load(100)
-	local result = c.FrameSearch(_do, 100)
-	
-	if result then
+	local result = c.Best(_do, 100)	
+	if result > 0 then
+		c.Save(7)
 		c.Done()
 	end
 end
