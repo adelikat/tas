6daa00548904179f449223d7531b035ac1a53265
adelikat <AndrewJDelikat@gmail.com>
2026-02-20 17:16:06 -0600

    more lode runner lua stuff

--- LodeRunner/Lua/lode-runner.lua
+++ LodeRunner/Lua/lode-runner.lua
@@ -1,5 +1,3 @@
---TODO: detect enemy is in ground, only show spawn circle during that time
--- use dig timers to try to predict where an enemy will land ahead of time, instead of cycling it
 -- use spawn timer to create a lag counter
 local function toSignedByte(b)
     if b > 127 then
@@ -31,12 +29,23 @@ local function getEnemy(n)
     return enemy
 end
 
+local function XcoordToScreen(x)
+    local camX = memory.readbyte(0x0004)
+    return (((x) * 16) - camX) + 14 + 2
+end
+
+local function YcoordToScreen(y)
+    return ((y - 1) * 16) + 8
+end
+
 local function drawEnemy(e)
     local camX = memory.readbyte(0x0004)
+    -- todo - use XcoordToScreen
     local x = (((e.levelX) * 16) - camX) + 14 + e.xTileOffset
     x = math.min(255, x)
     local x = math.max(-14, x)
 
+    -- todo - use YcoordToScreen
     y = ((e.levelY - 1) * 16) + 8 + e.yTileOffset
 
     gui.drawRectangle(x, y, 15, 15, e.color)
@@ -58,13 +67,25 @@ local function drawSpawnPrediction(num)
     if digCounter == 0 then
         return
     end
+    local digX = memory.readbyte(0x06A0 + i)
+    local digY = memory.readbyte(0x06C0 + i)
+
     local spawnTimer = memory.readbyte(0x0053)
+
+    local colors = {
+        [0] = '#D2B48C',
+        [1] = '#A67B5B',
+        [2] = '#7B4F2C',
+        [3] = '#4B2E1A',
+    }
 
     local camX = memory.readbyte(0x0004)
     local prediction = (spawnTimer + digCounter) % 32
-    gui.drawText(10, 12, spawnTimer, 'red')
-    gui.drawText(10, 0, digCounter, 'brown')
-
+    local x = XcoordToScreen(prediction)
+    local y = YcoordToScreen(1)
+    local color = colors[i] or '#4B2E1A';
+    gui.drawEllipse(x + 2, y, 15, 15, color)
+    gui.drawRectangle(XcoordToScreen(digX), YcoordToScreen(digY), 15, 15, color)
 end
 
 while true do
@@ -72,5 +93,8 @@ while true do
     drawEnemy(getEnemy(2))
     drawEnemy(getEnemy(3))
     drawSpawnPrediction(1)
+    drawSpawnPrediction(2)
+    drawSpawnPrediction(3)
+    drawSpawnPrediction(4)
 	emu.frameadvance();
 end
