40a20cd02fa13b0b652077cfd5bb592312b8eb8f
adelikat <AndrewJDelikat@gmail.com>
2026-03-14 12:32:27 -0500

    progress on input minmizer script

---
+++ LodeRunner/Lua/nes-input-minimizer-tastudio.lua
@@ -0,0 +1,178 @@
+local verboseLogging = false
+local targetFrame = 20 -- how many frames to advance to ensure memory is synced after changing inputs
+local mode = movie.mode()
+
+if not tastudio.engaged() then
+    error('Tastudio needs to be engaged')
+end
+
+if mode ~= 'PLAY' then
+    error('Movie needs to be in playback mode, current mode: ' .. mode)
+end
+
+local _rawBtnList = {
+    ['P1 Up'] = false,
+    ['P1 Down'] = false,
+    ['P1 Left'] = false,
+    ['P1 Right'] = false,
+    ['P1 B'] = false,
+    ['P1 A'] = false,
+    ['P1 Select'] = false,
+    ['P1 Start'] = false,
+}
+
+function Log(msg)
+    console.log(msg)
+end
+
+--use console log, for debugging, support params like console.log does
+function Debug(str)
+    if verboseLogging then
+        console.log('debug--')
+        console.log(str)
+    end
+end
+
+function getAllMemory()
+    return mainmemory.read_bytes_as_array(0x0000, 0x07FF)
+end
+
+local function getPressedBtns(btns)
+    local pressed = {}
+
+    for k, v in pairs(btns or {}) do
+        if v == "True" or v == true then
+            pressed[k] = v
+        end
+    end
+
+    return pressed
+end
+
+local function areArraysTheSame(arr1, arr2)
+    if #arr1 ~= #arr2 then
+        Log('Error - arrays are different lengths: ' .. #arr1 .. ' vs ' .. #arr2)
+        return false
+    end
+
+    for k, v in pairs(arr1) do
+        if arr2[k] ~= v then
+            Debug('difference found in byte ' .. k .. ': ' .. string.format("%02X", v) .. ' vs ' .. string.format("%02X", arr2[k]))
+            return false
+        end
+    end
+
+    return true
+end
+
+local function getMarkerbefore(frame)
+    local frames = tastudio.get_frames_with_markers()
+    local lastFrame = nil
+
+    for k, v in pairs(frames) do
+        if v < frame then
+            lastFrame = v
+        else
+            break
+        end
+    end
+
+    return lastFrame
+end
+
+local function getMarkerAfter(frame)
+    local frames = tastudio.get_frames_with_markers()
+
+    for k, v in pairs(frames) do
+        if v > frame then
+            return v
+        end
+    end
+end
+
+function navigateTo(frame)
+    client.unpause()
+    tastudio.setplayback(frame)
+    while emu.framecount() < frame do
+        emu.frameadvance()
+    end
+    client.pause()
+end
+
+local function isAnyBtnPressed(btns)
+    for k, v in pairs(btns) do
+        if v == "True" or v == true then
+            return true
+        end
+    end
+
+    return false
+end
+
+local function minimizeButtonsOnFrame(frame, currentFrame)
+    local btns = movie.getinput(frame)
+    Debug('btns for frame ' .. frame)
+    Debug(btns)
+    if not isAnyBtnPressed(btns) then
+        Log('no buttons pressed for frame ' .. frame)
+        return
+    end
+
+    navigateTo(frame + targetFrame)
+    local syncedMemory = getAllMemory()
+    navigateTo(frame)
+
+    for k, v in pairs(btns) do
+        tastudio.submitinputchange(frame, k, false)
+        tastudio.applyinputchanges()
+
+        navigateTo(frame + targetFrame)
+        local newMemory = getAllMemory()
+        local result = areArraysTheSame(syncedMemory, newMemory)
+        if result then
+            Log('Sync Success! keeping ' .. k .. ' off')
+        else
+            Log('Sync Failed! ' .. k .. ' is needed')
+            tastudio.submitinputchange(frame, k, true)
+            tastudio.applyinputchanges()
+        end
+    end
+
+
+end
+
+local currentFrame = emu.framecount()
+Log('starting frame', currentFrame)
+local startFrame = getMarkerbefore(currentFrame)
+if startFrame == nil then
+    error('No marker found before current frame')
+end
+
+local endFrame = getMarkerAfter(currentFrame)
+if endFrame == nil then
+    endFrame = movie.length() - 1
+end
+
+Log('processing frames between ' .. startFrame .. ' and ' .. endFrame)
+
+local done = false
+client.unpause()
+client.speedmode(1600)
+local currentFrame = startFrame
+while not done do
+    navigateTo(currentFrame)
+    minimizeButtonsOnFrame(currentFrame)
+    currentFrame = currentFrame + 1
+
+    if currentFrame > endFrame then
+        done = true
+    else
+        Log('advancing to ' .. currentFrame)
+    end
+end
+
+client.speedmode(100)
+client.pause()
+Log('------------')
+Log('DONE')
+Log('------------')
--- LodeRunner/Lua/nes-input-minimizer.lua
+++ LodeRunner/Lua/nes-input-minimizer.lua
@@ -4,10 +4,6 @@ if mode ~= 'PLAY' then
     error('Movie needs to be in playback mode, current mode: ' .. mode)
 end
 
-client.unpause()
-
-local done = false
-
 local _rawBtnList = {
     ['P1 Up'] = false,
     ['P1 Down'] = false,
@@ -23,7 +19,7 @@ function getAllMemory()
     return mainmemory.read_bytes_as_array(0x0000, 0x07FF)
 end
 
-local function save(slot)
+local function saveS(slot)
     if slot == nil then
         error("slot can not be nil")
     end
@@ -44,6 +40,24 @@ local function save(slot)
     end
 end
 
+ function loadS(slot)
+    if slot == nil then
+        error("slot must be a number")
+    end
+
+    slotNum = tonumber(slot)
+
+    if slotNum == 0 then
+        slotNum = 10
+    end
+
+    if slotNum ~= nil and slotNum > 0 and slotNum <= 10 then
+        savestate.loadslot(slotNum)
+    else
+        savestate.load(string.format('state-archive/%s.State', slot))
+    end
+end
+
 local function getPressedBtns(btns)
     local pressed = {}
 
@@ -58,12 +72,10 @@ local function getPressedBtns(btns)
 end
 
 local function areArraysTheSame(arr1, arr2)
-    if #arr1 ~= #arr2 then
-        return false
-    end
-
     for k, v in pairs(arr1) do
+        --console.log('comparing ' .. string.format("%02X", v) .. ' to ' .. string.format("%02X", arr2[k])))
         if arr2[k] ~= v then
+            console.log('difference found in byte ' .. k .. ': ' .. string.format("%02X", v) .. ' vs ' .. string.format("%02X", arr2[k]))
             return false
         end
     end
@@ -71,38 +83,119 @@ local function areArraysTheSame(arr1, arr2)
     return true
 end
 
-local function minimizeBtnSet(btns, frame)
-    save('minimize-start-' .. frame)
-    console.log('getting memory for frame ' .. frame)
-    local newBtns = _rawBtnList;
-    local origMemory = getAllMemory()
-    for k, v in pairs(btns) do
-        newBtns[k] = false
-        console.log('turning off ' .. k .. ' new btns', newBtns)
+local function advance(numFrames)
+    client.unpause()
+    for i = 1, numFrames do
+        emu.frameadvance()
+    end
+    client.pause()
+end
 
+local function isAnyBtnPressed(btns)
+    for k, v in pairs(btns) do
+        if v == "True" or v == true then
+            return true
+        end
+    end
+
+    return false
+end
+
+local function writeArrayToFile(arr, filename)
+    local file = io.open(filename, "w")
+    for k, v in pairs(arr) do
+        file:write(string.format("%02X\n", v))
+    end
+    file:close()
+end
+
+local function minimizeBtnSet(btns, frame)
+    local saveStateName = 'minimize-start-' .. frame
+    if not isAnyBtnPressed(btns) then
+        console.log('no buttons pressed for frame ' .. frame)
+        return
+    end
+
+    console.log('minimizing btns for frame ' .. frame)
+
+    saveS(saveStateName)
+    advance(10)
+    local syncedMemory = getAllMemory()
+    writeArrayToFile(syncedMemory, 'memory-' .. frame .. 'before-minimize.txt')
+
+    loadS(saveStateName)
+
+    local keepBtns = _rawBtnList;
+
+    for k, v in pairs(btns) do
+        console.log('turning off ' .. k)
+        local tryBtns = btns;
+        tryBtns[k] = false
+        joypad.set(tryBtns)
         client.unpause()
         emu.frameadvance()
         client.pause()
-        console.log('now getting getting memory for next frame ' .. emu.framecount())
+        advance(10)
         local newMemory = getAllMemory()
-        local result = areArraysTheSame(origMemory, newMemory)
+        writeArrayToFile(newMemory, 'memory-' .. frame .. 'after-minimize.txt')
+        local result = areArraysTheSame(syncedMemory, newMemory)
+        if (result) then
+            console.log('Sync Success! keeping ' .. k .. ' off')
+            keepBtns[k] = false
+        else
+            console.log('desync, reverting ' .. k)
+            keepBtns[k] = true
+        end
 
-        console.log('memory is the same', result)
+        loadS(saveStateName)
     end
+
+    return keepBtns;
 end
 
+local function getAllMovieInputs()
+    local numFrames = movie.length()
+    local inputs = {}
+
+    for i = 0, numFrames - 1 do
+        inputs[i] = movie.getinput(i)
+    end
+
+    return inputs
+end
+
+local done = false
+local origInputs = getAllMovieInputs()
+local totalFrames = #origInputs
+
+movie.setreadonly(false)
+saveS('pre-minimize')
+loadS('pre-minimize')
+--client.unpause()
+client.speedmode(1600)
 while not done do
     local frame = emu.framecount()
-    local btns = movie.getinput(frame)
-    local pressed = getPressedBtns(btns)
-    minimizeBtnSet(pressed, frame)
-    console.log('btns', btns)
-    console.log('pressed', pressed)
+    local inputsToRecord = origInputs[frame]
+    if inputsToRecord == nil then
+        error('got nil for inputs for frame ' .. frame)
+    end
 
+    local pressed = getPressedBtns(origInputs[frame])
+    if isAnyBtnPressed(pressed) then
+        inputsToRecord = minimizeBtnSet(pressed, frame)
+    else
+        console.log('no buttons pressed for frame ' .. frame)
+    end
+
+    --client.unpause()
+    joypad.set(inputsToRecord)
     emu.frameadvance()
-    done = true
-end
 
+    if emu.framecount() >= totalFrames then
+        done = true
+    end
+end
+client.speedmode(100)
 client.pause()
 console.log('------------')
 console.log('success')
