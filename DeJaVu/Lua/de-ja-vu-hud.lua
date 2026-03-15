99288057844be3becbd9d2ede1a35cee4f9e6561
adelikat <AndrewJDelikat@gmail.com>
2026-03-15 13:53:02 -0500

    de ja vu - progress, 6 frames ahead after the slot machine

--- DeJaVu/Lua/de-ja-vu-hud.lua
+++ DeJaVu/Lua/de-ja-vu-hud.lua
@@ -29,5 +29,23 @@ while true do
     drawCursor(previousCursorX, previousCursorY, "gray")
     drawCursor(cursorX, cursorY, "white")
 
+    -- draw pointers to the coordinatese the cursor will land on from each angle
+
+    -- from right
+    gui.drawLine(119, 80, 135, 100, "gray")
+    gui.drawLine(119, 80, 135, 60, "gray")
+    gui.drawPixel(119, 80, "black")
+
+    -- from bottom
+    gui.drawLine(71, 128, 87, 144, "gray")
+    gui.drawLine(71, 128, 55, 144, "gray")
+    gui.drawPixel(71, 128, "black")
+
+    -- from top
+    gui.drawLine(71, 32, 87, 16, "gray")
+    gui.drawLine(71, 32, 55, 16, "gray")
+    gui.drawPixel(71, 32, "black")
+
+
 	emu.frameadvance();
 end
