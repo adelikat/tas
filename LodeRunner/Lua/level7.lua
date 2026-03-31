0e083efacfe250945cd0fca6c126f7154eb8bf3a
adelikat <AndrewJDelikat@gmail.com>
2026-03-30 20:43:38 -0500

    lode runner - lv 7 done, 27 ahead

--- LodeRunner/Lua/level7.lua
+++ LodeRunner/Lua/level7.lua
@@ -1,6 +1,16 @@
+-- Starts on the frame immediately after pressing select to do the select skip
 dofile('lode-runner-core.lua')
 
 c.Start()
+
+if c.CurrentLevel() ~= 7 then
+    error('must be run in level 7')
+end
+
+if c.GameMode() ~= 1 then
+    error('must be run in level mode')
+end
+
 
 local function ClimbAndDigLeft()
     local result = c.ClimbUntil(3)
@@ -9,7 +19,7 @@ local function ClimbAndDigLeft()
     if not result then return false end
     result = c.UntilDig('Left', 'B')
     if not result then return false end
-    return c.UntilFall('Left')
+    return c.FallLeft()
 end
 
 local function LeftFor2()
@@ -22,90 +32,62 @@ end
 while not c.IsDone() do
     c.RightUntil(18)
     c.UntilDig('Right', 'A')
-    c.FrameSearch(function() return c.UntilLadderGrab('Right') end)
-    c.Climb(2)
+    c.FrameSearch(c.GrabLadderRight)
+    c.ClimbUntil(10)
     c.PushUp()
 
-    c.FrameSearch(function() return c.Climb(4) end, 25)
+    c.FrameSearch(function() return c.ClimbUntil(6) end, 25)
     c.ClimbUntil(1)
     c.RightFor(5)
     c.PushDown()
     c.FinishFalling()
 
-    --
-    -- c.LeftFor(1)
-    -- c.UntilDig('Left', 'B')
-    -- c.UntilDig('Right', 'B')
-    -- c.UntilFall('Left')
-    -- c.FinishFalling()
-    -- c.UntilDig('Left', 'A')
-    -- c.UntilFall('Right')
-    -- c.FinishFalling()
-    -- c.UntilDig('Left', 'B')
-    -- c.UntilFall('Left')
-    -- c.FinishFalling()
-    -- c.UntilLadderGrab('Left')
-
-    -- Alternate dig pattern, 1 frame slower
-    -- c.UntilDig('Left', 'A')
-    -- c.UntilDig('Left', 'A')
-    -- c.UntilFall('Right')
-    -- c.FinishFalling()
-    -- c.UntilDig('Right', 'B')
-    -- c.UntilFall('Left')
-    -- c.FinishFalling()
-    -- c.UntilDig('Left', 'B')
-    -- c.UntilFall('Left')
-    -- c.FinishFalling()
-    -- c.UntilLadderGrab('Left')
-
-    -- Alt alt, somehow less lag if the enemy never falls into a hole
     c.LeftFor(2)
     c.UntilDig('Left', 'A')
     c.UntilDig('Left', 'A')
-    c.UntilFall('Right')
-    c.FinishFalling()
+    c.FallRight()
     c.UntilDig('Right', 'A')
-    c.UntilFall('Right')
-    c.FinishFalling()
+    c.FallRight()
     c.UntilDig('Left', 'B')
-    c.UntilFall('Left')
-    c.FinishFalling()
-    c.UntilLadderGrab('Left')
+    c.FallLeft()
+    c.GrabLadderLeft()
 
     c.ClimbUntil(7)
 
     c.FrameSearch(ClimbAndDigLeft)
-    c.FinishFalling()
 
-    c.UntilLadderGrab('Left')
+    c.GrabLadderLeft()
     c.ClimbUntil(7)
-    c.LeftFor(4)
 
-    -- The luck happend too work out but waiting here does not cost any frames, if spawn manip is needed
-    -- c.WaitFor(1)
+    c.LeftFor(3)
+
+    -- Must get good  enough spawn, 11 thru 19
+    c.LeftFor(1)
+    c.WaitFor(5) -- At least we have 5 frames of leeway
 
     c.UntilDig('Left', 'B')
-    c.WaitFor(5) -- speed optimization
+    c.WaitFor(5)
     c.FrameSearch(LeftFor2)
-    --c.LeftUntil(3)
-    --c.UntilGold('Left')
-    c.LeftUntil(2) -- This happened to get the gold due to how fast the game happened to be running, and the above code failed
+    c.LeftUntil(4)
+
+    -- GrabLadderLeft misses the gold
+    -- But until gold gets it but runs past the ladder
+    -- since there is no lag here this should be consistent
+    c.PushFor('Left', 4)
+    c.PushBtnsFor({'Left', 'Down'})
 
     c.ClimbUntil(9)
-    c.UntilFall('Right')
-    c.FinishFalling()
-    c.UntilGold('Right')
-    c.LeftFor(2)
-    c.UntilLadderGrab('Left')
+    c.FallRight()
+    c.UntilGoldRight()
+    c.GrabLadderLeft()
     c.ClimbUntil(7)
-    c.UntilLadderGrab('Right')
+    c.GrabLadderRight()
     c.ClimbUntil(5)
-    c.UntilGold('Right')
-    c.UntilLadderGrab('Left')
+    c.UntilGoldRight()
+    c.GrabLadderLeft()
     c.ClimbUntil(2)
     c.LeftFor(1)
-    c.UntilLadderGrab('Left')
+    c.GrabLadderLeft()
     c.ClimbUntilLevelEnd()
 
     c.Marker('lv 7 end')
