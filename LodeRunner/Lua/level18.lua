0ad1f56dc3bc6c7aae95244c6f42d1151de3b2f2
adelikat <AndrewJDelikat@gmail.com>
2026-03-31 18:24:05 -0500

    lode runner - merge v7.1 as v7 and rework files and spreadsheet, 244 frames ahead ahead of v6 at the start of level 15

--- LodeRunner/Lua/level18.lua
+++ LodeRunner/Lua/level18.lua
@@ -1,6 +1,15 @@
+-- Starts on the frame immediately after pressing select to do the select skip
 dofile('lode-runner-core.lua')
 
 c.Start()
+
+if c.CurrentLevel() ~= 18 then
+    error('must be run in level 18')
+end
+
+if c.GameMode() ~= 1 then
+    error('must be run in level mode')
+end
 
 while not c.IsDone() do
     c.Done()
