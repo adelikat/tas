4496691e5551dad0d60762c2fe3ca05baa8d8ccb
adelikat <AndrewJDelikat@gmail.com>
2026-04-02 09:02:29 -0500

    lode runner - lv 17 script

--- LodeRunner/Lua/level17.lua
+++ LodeRunner/Lua/level17.lua
@@ -12,109 +12,114 @@ if c.GameMode() ~= 1 then
 end
 
 while not c.IsDone() do
-    -- c.FallLeft()
-    -- c.GrabLadderLeft()
-    -- c.ClimbUntil(1)
-    -- c.FallRight()
-    -- c.RightFor(1)
-    -- c.UntilDig('Right', 'B')
-    -- c.FallLeft()
-    -- c.LeftFor(1)
-    -- c.FallLeft()
-    -- c.GrabLadderLeft()
-    -- c.ClimbUntil(1)
-    -- c.RightUntil(3)
+    c.FallLeft()
+    c.GrabLadderLeft()
+    c.ClimbUntil(1)
+    c.FallRight()
+    c.RightFor(1)
+    c.UntilDig('Right', 'B')
+    c.FallLeft()
+    c.LeftFor(1)
+    c.FallLeft()
+    c.GrabLadderLeft()
+    c.ClimbUntil(1)
+    c.RightUntil(3)
 
-    -- c.WaitFor(8)
-    -- c.WalkOverEnemy('Right')
+    c.WaitFor(8)
+    c.WalkOverEnemy('Right')
 
-    -- c.RightUntil(8)
-    -- c.UntilDig('Right', 'A')
-
-    -- c.Marker('temp')
-
+    c.RightUntil(8)
+    c.UntilDig('Right', 'A')
+    c.UntilDig('Left', 'A')
     c.UntilDig('Left', 'A')
 
-    -- c.UntilDig('Left', 'A')
-    -- c.FallRight()
-    -- c.RightFor(1)
-    -- c.UntilDig('Right', 'A')
-    -- c.UntilDig('Left', 'A')
-    -- c.FallRight()
-    -- c.UntilDig('Right', 'B')
-    -- c.FallLeft()
+    c.FallRight()
+    c.RightFor(1)
+    c.UntilDig('Right', 'A')
+    c.UntilDig('Left', 'A')
+    c.FallRight()
+    c.UntilDig('Right', 'B')
+    c.FallLeft()
 
-    -- c.FallLeft()
-    -- c.UntilDig('Left', 'A')
-    -- c.FallRight()
-    -- c.GrabLadderRight()
-    -- c.ClimbUntil(11)
-    -- c.UntilGoldRight()
-    -- c.UntilDig('Left', 'A')
+    c.FallLeft()
+    c.UntilDig('Left', 'A')
+    c.FallRight()
+    c.GrabLadderRight()
+    c.ClimbUntil(11)
+    c.UntilGoldRight()
+    c.UntilDig('Left', 'A')
 
-    -- c.WalkOverEnemy('Right')
-    -- c.ClimbUntil(8)
-    -- c.GrabLadderLeft()
-    -- c.ClimbUntil(4)
+    c.WalkOverEnemy('Right')
+    c.ClimbUntil(8)
+    c.GrabLadderLeft()
+    c.ClimbUntil(4)
 
-    -- c.BestOf({
-    --     function()
-    --         c.RightUntil(17)
-    --         c.PushDown()
-    --         c.FinishFalling()
-    --         return c.UntilDig('Left', 'B')
-    --     end,
-    --     function()
-    --         c.RightFor(2)
-    --         c.PushDown()
-    --         c.FinishFalling()
-    --         c.UntilGoldRight()
-    --         return c.UntilDig('Left', 'B')
-    --     end,
-    -- })
+    c.BestOf({
+        function()
+            c.RightUntil(17)
+            c.PushDown()
+            c.FinishFalling()
+            return c.UntilDig('Left', 'B')
+        end,
+        function()
+            c.RightFor(2)
+            c.PushDown()
+            c.FinishFalling()
+            c.UntilGoldRight()
+            return c.UntilDig('Left', 'B')
+        end,
+    })
 
-    -- c.FallLeft()
-    -- c.ClimbUntil(11)
-    -- c.UntilDig('Right', 'A')
-    -- c.FallRight()
-    -- c.GrabLadderRight()
+    c.FallLeft()
+    c.ClimbUntil(11)
+    c.UntilDig('Right', 'A')
+    c.FallRight()
+    c.GrabLadderRight()
 
-    -- c.ClimbUntil(11)
-    -- c.RightUntil(22)
-    -- c.UntilDig('Right', 'A')
-    -- c.WalkOverEnemy('Right')
-    -- c.GrabLadderRight()
-    -- c.ClimbUntil(5)
-    -- c.LeftFor(1)
-    -- c.UntilDig('Left', 'B')
-    -- c.UntilDig('Right', 'B')
-    -- c.FallLeft()
-    -- c.UntilDig('Left', 'B')
-    -- c.FallLeft()
-    -- c.UntilGoldLeft()
-    -- c.UntilDig('Right', 'B')
-    -- c.FallLeft()
-    -- c.GrabLadderRight()
+    c.ClimbUntil(11)
+    c.RightUntil(22)
+    c.UntilDig('Right', 'A')
+    c.WalkOverEnemy('Right')
+    c.GrabLadderRight()
+    c.ClimbUntil(5)
+    c.LeftFor(1)
+    c.UntilDig('Left', 'B')
+    c.UntilDig('Right', 'B')
+    c.FallLeft()
+    c.UntilDig('Left', 'B')
 
-    -- c.ClimbUntil(10)
+    c.Assert(c.FrameSearch(function()
+        c.FallLeft()
+        c.UntilGoldLeft()
+        c.UntilDig('Right', 'B')
+        c.FallLeft()
+        local result = c.GrabLadderRight()
+        if not result then return false end
+        return c.ClimbUntil(10)
+    end))
 
     -- c.Assert(c.FrameSearch(function()
     --     local result = c.ClimbUntil(8)
-    --         if not result then return false end
-    --         c.PushFor('Up', 2)
+    --     if not result then return false end
+    --     c.PushUp()
 
-    --         local result = c.Player().isAlive
-    --         if result and c.Player().yPos() ~= 8 then
-    --             error('this search was only tested on yPos of exactly 8')
-    --         end
-    --         return result
+    --     local result = c.Player().isAlive and c.Player().yPos() < 8.5
+    --     return result
     --     end
     -- ))
 
     -- c.Assert(c.FrameSearch(function() return c.ClimbUntil(2) end))
-    -- c.ClimbUntilLevelEnd()
 
-    -- c.Marker('lv 17 end')
+    -- This hardcoded pattern was 1 frame faster than what the search patterns found
+    c.WaitFor(3)
+    c.PushFor('Up', 4)
+    c.WaitFor(1)
+    c.PushFor('Up', 2)
+    c.WaitFor(4)
+
+    c.ClimbUntilLevelEnd()
+
+    c.Marker('lv 17 end')
 
     c.Done()
 end
