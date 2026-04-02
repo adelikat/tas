9d83a979c8b0218b25702a212508b3be2841f1c5
adelikat <AndrewJDelikat@gmail.com>
2026-04-02 18:10:41 -0500

    lode runner - lv 19 done - 349 ahead

--- LodeRunner/Lua/level19.lua
+++ LodeRunner/Lua/level19.lua
@@ -12,6 +12,53 @@ if c.GameMode() ~= 1 then
 end
 
 while not c.IsDone() do
+    c.RightUntil(22)
+    c.GrabLadderRight()
+
+    c.WaitFor(3) -- Ensure E3 picks up gold, and E1 will continue to walk left
+
+    c.ClimbUntil(9)
+    c.LeftFor(1)
+    c.GrabLadderLeft()
+    c.ClimbUntil(7)
+    c.LeftFor(1)
+    c.GrabLadderLeft()
+    c.ClimbUntil(5)
+    c.LeftFor(1)
+    c.GrabLadderLeft()
+    c.ClimbUntil(3)
+    c.UntilGoldLeft()
+
+    c.GrabLadderRight('Down')
+    c.ClimbUntil(5)
+    c.GrabLadderRight('Down')
+    c.ClimbUntil(7)
+
+    c.UntilGoldLeft()
+    c.UntilGoldLeft()
+    c.UntilGoldLeft()
+
+    c.UntilDig('Right', 'B')
+    c.UntilDig('Right', 'B')
+    c.UntilDig('Right', 'B')
+
+    c.Assert(c.WalkOverEnemy('Left'))
+    c.LeftUntil(7)
+    c.GrabLadderLeft()
+    c.ClimbUntil(5)
+    c.RightFor(1)
+    c.GrabLadderRight()
+    c.ClimbUntil(3)
+    c.RightFor(1)
+    c.GrabLadderRight()
+    c.ClimbUntil(1)
+    c.UntilGoldRight()
+    c.UntilGoldRight()
+    c.GrabLadderLeft()
+    c.ClimbUntilLevelEnd()
+
+    c.Marker('lv 19 end')
+
     c.Done()
 end
 
