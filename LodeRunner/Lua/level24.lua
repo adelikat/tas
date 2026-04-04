cfd0d577611ff4f12a7ca3eaab9e0d4d6db3b65d
adelikat <AndrewJDelikat@gmail.com>
2026-04-03 21:17:33 -0500

    level 24 done, 410 ahead

--- LodeRunner/Lua/level24.lua
+++ LodeRunner/Lua/level24.lua
@@ -12,6 +12,86 @@ if c.GameMode() ~= 1 then
 end
 
 while not c.IsDone() do
+    c.GrabLadderLeft()
+    c.ClimbUntil(8)
+    c.UntilDig('Left', 'B')
+    c.ClimbUntil(9)
+    c.UntilDig('Left', 'B')
+    c.BestOf({
+        function() return c.UntilGoldLeft() end,
+        function()
+            c.ClimbUntil(10)
+            return c.UntilGoldLeft()
+        end
+    })
+
+    c.GrabLadderLeft()
+    c.ClimbUntil(9)
+    c.UntilDig('Left', 'B')
+    c.WalkOverEnemy('Left')
+    c.GrabLadderLeft('Down')
+    c.ClimbUntil(11)
+    c.UntilGoldRight()
+    c.LeftFor(2)
+    c.GrabLadderLeft()
+    c.ClimbUntil(9)
+    c.UntilGoldLeft()
+    c.GrabLadderRight('Down')
+    c.ClimbUntil(11)
+    c.UntilGoldLeft()
+
+    c.GrabLadderLeft()
+    c.ClimbUntil(10)
+    c.LeftFor(1)
+    c.UntilDig('Left', 'B')
+    c.WalkOverEnemy('Left')
+    c.LeftFor(1)
+    c.UntilDig('Left', 'A')
+    c.ClimbUntil(9)
+    c.UntilDig('Left', 'B')
+    c.ClimbUntil(10)
+    c.UntilDig('Left', 'B')
+    c.UntilGoldLeft()
+
+    c.GrabLadderRight()
+    c.ClimbUntil(10)
+    c.Assert(c.FrameSearch(function() return c.RightFor(3) end))
+    c.GrabLadderRight('Down')
+    c.ClimbUntil(11)
+    c.RightUntil(16)
+    c.GrabLadderRight()
+    c.ClimbUntil(6)
+    c.FallLeft()
+    c.GrabLadderLeft()
+    c.ClimbUntil(5)
+    c.UntilGoldLeft()
+    c.UntilDig('Right', 'B')
+    c.ClimbUntil(6)
+    c.UntilDig('Right', 'B')
+    c.UntilGoldLeft()
+    c.UntilGoldLeft()
+    c.GrabLadderRight()
+    c.ClimbUntil(5)
+
+    c.Assert(c.FrameSearch(function() return c.GrabLadderLeft() end))
+    c.ClimbUntil(3)
+    c.GrabLadderLeft()
+
+    c.BestOf({
+        function()
+            c.ClimbUntil(1)
+            c.GrabLadderRight()
+            return c.ClimbUntilLevelEnd()
+        end,
+        -- function()
+        --     c.ClimbUntil(2)
+        --     c.GrabLadderRight()
+        --     return c.ClimbUntilLevelEnd()
+        -- end,
+    })
+
+    c.Marker('lv 24 end')
+
     c.Done()
 end
 
