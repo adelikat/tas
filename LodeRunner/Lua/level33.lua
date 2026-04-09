eac56eb36d66bac06f473524873a263242e336e7
adelikat <AndrewJDelikat@gmail.com>
2026-04-09 16:16:52 -0500

    lv 33 done (select skip not manipulated yet) - 560 ahead

--- LodeRunner/Lua/level33.lua
+++ LodeRunner/Lua/level33.lua
@@ -12,6 +12,81 @@ if c.GameMode() ~= 1 then
 end
 
 while not c.IsDone() do
+    c.UntilGoldRight()
+    c.LeftFor(6)
+    c.GrabLadderLeft()
+    c.ClimbUntil(8)
+    c.UntilGoldRight()
+    c.GrabLadderLeft()
+    c.ClimbUntil(4)
+    c.RightUntil(9)
+    c.BestOf({
+        function()
+            c.UntilGoldRight()
+            return c.GrabLadderLeft()
+        end,
+        function()
+            c.GrabLadderRight('Down')
+            c.ClimbUntil(5)
+            c.UntilGoldRight()
+            return c.GrabLadderLeft()
+        end
+    })
+
+    c.ClimbUntil(4)
+    c.LeftFor(1)
+    c.GrabLadderLeft('Down')
+    c.ClimbUntil(7)
+    c.FinishFalling()
+    c.FallRight()
+    c.RightUntil(14)
+    c.UntilDig('Right', 'A')
+    c.RightUntil(26)
+
+    c.BestSearch(function()
+        c.GrabLadderRight()
+        c.ClimbUntil(11)
+        c.LeftFor(1)
+        local result = c.UntilDig('Left', 'B')
+        if not result then return false end
+        result = c.UntilDig('Right', 'B')
+        if not result then return false end
+        result = c.WalkOverEnemy('Left')
+        if not result then return false end
+        c.LeftFor(1)
+        c.GrabLadderLeft()
+        c.ClimbUntil(1)
+        c.RightFor(1)
+        c.UntilDig('Right', 'A')
+        c.FallRight()
+        c.UntilDig('Right', 'A')
+        c.FallRight()
+        result = c.LeftUntil(16)
+        if not result then return false end
+        result = c.GrabLadderLeft()
+        if not result then return false end
+        result = c.ClimbUntil(1)
+        if not result then return false end
+        c.RightUntil(17)
+        c.UntilDig('Right', 'A')
+        c.UntilDig('Left', 'A')
+        c.FallRight()
+        c.UntilDig('Right', 'A')
+        c.FallRight()
+        c.UntilDig('Right', 'B')
+        c.FallLeft()
+        c.GrabLadderLeft()
+        c.ClimbUntil(1)
+        c.Assert(c.LeftUntil(2))
+        c.GrabLadderLeft()
+        c.ClimbUntil(1)
+        c.RightUntil(2)
+        c.GrabLadderRight()
+        return c.ClimbUntilLevelEnd()
+    end, 10)
+
+    c.Marker('lv 33 end')
+
     c.Done()
 end
 
