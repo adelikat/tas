4c519190f4e53d3234f4d7cd434cea2e8394a3b4
adelikat <AndrewJDelikat@gmail.com>
2026-04-18 12:42:43 -0500

    lode runner - lv 43 done - 701 ahead

--- LodeRunner/Lua/level43.lua
+++ LodeRunner/Lua/level43.lua
@@ -12,6 +12,117 @@ if c.GameMode() ~= 1 then
 end
 
 while not c.IsDone() do
+    c.UntilDig('Right', 'B')
+    c.BestOf({
+        function()
+            c.UntilGoldLeft()
+            c.GrabAndClimbOneRight()
+            c.RightFor(1)
+            return c.GrabLadderRight()
+        end,
+        function()
+            c.ClimbUntil(13)
+            c.UntilGoldLeft()
+            c.GrabAndClimbOneRight()
+            c.RightFor(1)
+            return c.GrabLadderRight()
+        end,
+    })
+
+    c.ClimbUntil(8)
+    c.RightFor(1)
+    c.GrabAndClimbOneRight()
+    c.UntilGoldRight()
+
+    c.GrabLadderLeft()
+    c.ClimbUntil(5)
+    c.LeftFor(1)
+    c.GrabLadderLeft()
+    c.ClimbUntil(3)
+    c.LeftFor(1)
+    c.GrabLadderLeft()
+    c.ClimbUntil(1)
+    c.LeftUntil(13)
+    c.UntilDig('Left', 'B')
+    c.FallLeft()
+
+        c.WaitFor(2) -- Manip enemy spawn to be far to the right
+    c.UntilDig('Right', 'A')
+    c.WalkOverEnemy('Right')
+    c.RightUntil(17)
+    c.UntilDig('Right', 'B')
+    c.FallLeft()
+    c.ClimbUntil(6)
+    c.UntilGoldLeft()
+
+    c.BestOf({
+        function()
+            c.FallLeft()
+            c.FallLeft()
+            return c.GrabLadderLeft()
+        end,
+        function()
+            c.GrabLadderLeft('Down')
+            c.ClimbUntil(7)
+            c.LeftFor(1)
+            c.GrabLadderLeft('Down')
+            c.ClimbUntil(8)
+            c.UntilGoldLeft()
+            return c.GrabLadderLeft()
+        end,
+        function()
+            c.FallLeft()
+            c.GrabLadderLeft('Down')
+            c.ClimbUntil(8)
+            c.UntilGoldLeft()
+            return c.GrabLadderLeft()
+        end,
+        function()
+            c.GrabLadderLeft('Down')
+            c.ClimbUntil(7)
+            c.LeftFor(1)
+            c.FallLeft()
+            return c.GrabLadderLeft()
+        end,
+    })
+
+    c.ClimbUntil(7)
+    c.LeftFor(1)
+    c.GrabLadderLeft()
+
+    c.Assert(c.FrameSearch(function()
+        local result = c.ClimbUntil(5)
+        local result = c.LeftFor(1)
+        if not result then return false end
+        result = c.GrabLadderLeft()
+        if not result then return false end
+        result = c.ClimbUntil(3)
+        if not result then return false end
+        result = c.UntilGoldRight()
+        if not result then return false end
+        return c.LeftUntil(2)
+    end))
+
+    c.PushLeft()
+    c.PushLeft()
+
+    c.Assert(c.FrameSearch(function()
+        local result = c.FallLeft()
+        if not result then return false end
+        result = c.UntilGoldLeft()
+        if not result then return false end
+        return c.RightUntil(5)
+    end))
+
+    c.GrabLadderRight()
+    c.ClimbUntil(5)
+    c.LeftFor(1)
+    c.GrabLadderLeft()
+    c.ClimbUntil(3)
+    c.LeftFor(1)
+    c.GrabLadderLeft()
+    c.ClimbUntilLevelEnd()
+
     c.Done()
 end
 
