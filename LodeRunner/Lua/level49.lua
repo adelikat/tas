d8cb01603fbc57d4784fb432a9085b8a43716269
adelikat <AndrewJDelikat@gmail.com>
2026-04-27 19:48:47 -0500

    lode runner - level 49 nominally done - 892 ahead

--- LodeRunner/Lua/level49.lua
+++ LodeRunner/Lua/level49.lua
@@ -14,6 +14,129 @@ end
 while not c.IsDone() do
     c.WaitFor(16)
     c.FallLeft()
+    c.UntilDig('Right', 'B')
+    c.FallLeft()
+    c.LeftFor(2)
+    c.GrabLadderLeft()
+    c.ClimbUntil(8)
+    c.RightUntil(4)
+    c.WalkOverEnemy('Right') -- It is a pit, but it works
+    c.RightUntil(10)
+    c.UntilDig('Right', 'A')
+    c.ClimbUntil(9)
+    c.UntilDig('Right', 'A')
+    c.ClimbUntil(10)
+    c.UntilDig('Right', 'A')
+
+    c.ClimbUntil(8)
+    c.UntilGoldRight()
+
+    c.LeftUntil(12)
+    c.GrabLadderLeft('Down')
+
+    c.BestOf({
+        function()
+            c.ClimbUntil(12)
+            c.UntilGoldLeft()
+            c.UntilGoldLeft()
+            return c.GrabLadderRight()
+        end,
+        function()
+            c.ClimbUntil(11)
+            c.FallLeft()
+            c.UntilGoldLeft()
+            c.UntilGoldLeft()
+            return c.GrabLadderRight()
+        end,
+    })
+
+    c.ClimbUntil(11)
+    c.Assert(c.FallRight())
+
+    c.GrabLadderRight()
+    c.ClimbUntil(11)
+
+    c.Assert(c.FrameSearch(function()
+        c.ClimbUntil(12)
+
+        c.Save('49-temp')
+
+        c.WaitFor(10)
+
+        local e1 = c.Enemy(1)
+        local e2 = c.Enemy(2)
+        local result = e1.levelX == 19 and e1.levelY == 12
+        and e2.levelX == 18 and e2.levelY == 12
+
+        c.Load('49-temp')
+        return result
+    end))
+
+    c.Assert(c.FrameSearch(function()
+        c.ClimbUntil(11)
+        return c.GrabLadderRight()
+    end))
+
+    c.ClimbUntil(1)
+    c.UntilGoldRight()
+    c.UntilGoldRight()
+    c.GrabLadderLeft('Down')
+    c.ClimbUntil(3)
+    c.RightFor(3)
+    c.GrabLadderRight('Down')
+    c.ClimbUntil(7)
+    c.FinishFalling()
+    c.LeftFor(2)
+    c.GrabLadderLeft()
+    c.ClimbUntil(5)
+    c.FallRight()
+    c.PushDown()
+    c.FinishFalling()
+    c.LeftUntil(24)
+    c.UntilDig('Left', 'B')
+    c.FallLeft()
+    c.UntilDig('Left', 'A')
+    c.UntilDig('Left', 'A')
+
+    c.BestOf({
+        function()
+            c.FallRight()
+            c.UntilGoldRight()
+            c.UntilGoldRight()
+            c.LeftFor(2)
+            return c.GrabLadderLeft()
+        end,
+        function()
+            c.ClimbUntil(12)
+            c.UntilGoldRight()
+            c.UntilGoldRight()
+            c.LeftFor(2)
+            return c.GrabLadderLeft()
+        end,
+    })
+
+    c.UntilDig('Left', 'B')
+    c.ClimbUntil(6)
+    c.FallLeft()
+    c.RightUntil(18)
+    c.UntilDig('Right', 'A')
+    c.UntilDig('Left', 'A')
+    c.WalkOverEnemy('Right')
+    c.GrabLadderRight()
+    c.ClimbUntil(1)
+    c.LeftUntil(15)
+    c.FinishFalling()
+    c.GrabLadderLeft()
+    c.ClimbUntil(5)
+    c.LeftUntil(3)
+    c.UntilGoldLeft()
+    c.RightFor(5)
+    c.GrabLadderRight()
+    c.ClimbUntilLevelEnd()
+
+    c.Marker('lv 49 end')
+
+
     c.Done()
 end
 
